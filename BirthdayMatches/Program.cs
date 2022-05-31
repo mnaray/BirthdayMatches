0ea091a0d22640a6bb7e7f1b7004ab09214fa641
mnaray <m.naray.inf21@stud.bbbaden.ch>
2022-05-31 11:10:59 +0200

    added for loop to recall function

--- BirthdayMatches/Program.cs
+++ BirthdayMatches/Program.cs
@@ -7,9 +7,28 @@ namespace BirthdayMatches
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter amount of people to check.");
+            Console.WriteLine("Enter amount of people to check: ");
             int amountOfPeople = Int32.Parse(Console.ReadLine());
 
+            Console.WriteLine("Enter amount of repetitions to calculate the average from: ");
+            int amountOfRepetitions = Int32.Parse(Console.ReadLine());
+
+            for (int i = 0; i < amountOfRepetitions; i++)
+            {
+                BirthdayMatcher(amountOfPeople);
+                Console.WriteLine(Environment.NewLine);
+                Console.WriteLine("------------------------------------------------------------------------");
+                Console.WriteLine(Environment.NewLine);
+            }
+
+
+
+            Console.WriteLine("Press any key to continue . . .");
+            Console.ReadKey();
+        }
+
+        public static void BirthdayMatcher(int amountOfPeople)
+        {
             IDictionary<DateTime, int> people = new Dictionary<DateTime, int>();
 
             for (int i = 0; i < amountOfPeople; i++)
@@ -25,13 +44,10 @@ namespace BirthdayMatches
                     people.Add(person.birthday, 1);
                 }
 
-                Console.WriteLine(person.birthday + "     "+ people[person.birthday]);
+                Console.WriteLine(person.birthday + "   ||   " + people[person.birthday]);
             }
-
-
-
-            Console.ReadKey();
         }
+
     }
 
     class Person
